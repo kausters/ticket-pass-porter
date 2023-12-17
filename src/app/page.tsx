c36824f0c521d450dc78442e813826a6e280fff8
Kristaps <kristaps@austers.lv>
2023-12-17 18:20:43 +0200

    Add "View example" link, scope stuff under /ticket

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import Link from 'next/link';
 import styles from './page.module.css';
 
 export default function Home() {
 	return (
 		<main className={styles.main}>
-			<div className={styles.description}></div>
+			<Link href="/ticket/view/">View example</Link>
 		</main>
 	);
 }
