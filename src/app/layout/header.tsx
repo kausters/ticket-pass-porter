b76a748d1c0d361d60cd59b2f59d866bce9b2e61
Kristaps <kristaps@austers.lv>
2023-12-17 18:13:58 +0200

    Add Header to layout

---
+++ src/app/layout/header.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+import styles from './header.module.css';
+import logo from './logo.png';
+
+export default function Header() {
+	return (
+		<header className={styles.container}>
+			<Image src={logo} alt="Logo" className={styles.logo} />
+			<h1 className={styles.title}>Biļetnieks</h1>
+		</header>
+	);
+}
