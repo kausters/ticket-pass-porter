ce49442b3879797d8eeec1118e80bdcb2aa33370
Kristaps <kristaps@austers.lv>
2024-01-04 01:50:20 +0200

    Parse uploaded PDF on client

--- src/app/ticket/view/parse/client.ts
+++ src/app/ticket/view/parse/client.ts
@@ -1,9 +1,35 @@
 'use client';
 
 import { TicketInvoice } from '../tickets.model';
+import {
+	isValid as isMarkusTicket,
+	parse as parseMarkusTicket,
+} from './markus';
+
+async function SetupPdfJs(): Promise<
+	typeof import('pdfjs-dist/legacy/build/pdf.mjs')
+> {
+	return new Promise((resolve) => {
+		return Promise.all([
+			import('pdfjs-dist/legacy/build/pdf.mjs'),
+			// @ts-expect-error
+			import('pdfjs-dist/legacy/build/pdf.worker.mjs'),
+		]).then(([Pdf, PdfWorker]) => {
+			Pdf.GlobalWorkerOptions.workerSrc = PdfWorker;
+			resolve(Pdf);
+		});
+	});
+}
 
 export async function parse(file: File): Promise<TicketInvoice> {
-	console.log(file);
+	const { getDocument } = await SetupPdfJs();
 
-	return {} as TicketInvoice;
+	const arrayBuffer = await file.arrayBuffer();
+	const pdf = await getDocument(arrayBuffer).promise;
+
+	if (await isMarkusTicket(pdf)) {
+		return await parseMarkusTicket(pdf);
+	}
+
+	throw new Error('Invalid ticket');
 }
