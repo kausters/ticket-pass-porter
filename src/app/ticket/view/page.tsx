f93154a7e62f8a11932c37dbc3263c39506369a4
Kristaps <kristaps@austers.lv>
2024-01-04 02:15:11 +0200

    Add server parsing by ID (filename)

--- src/app/ticket/view/page.tsx
+++ src/app/ticket/view/page.tsx
@@ -1,18 +1,27 @@
 'use client';
 
+import { useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
+
 import { useTicketUpload } from '../ticket-upload-context';
 import { parse as parseClient } from './parse/client';
+import { parse as parseServer } from './parse/server';
 import { TicketInvoice } from './tickets.model';
 
 export default function Page() {
 	const { ticketFile } = useTicketUpload();
 	const [invoice, setInvoice] = useState<TicketInvoice>();
 
+	const searchParams = useSearchParams();
+
 	useEffect(() => {
 		const updateInvoice = async () => {
 			if (ticketFile) {
 				const updatedInvoice = await parseClient(ticketFile);
+				setInvoice(updatedInvoice);
+			} else if (searchParams.has('id')) {
+				const ticketId = searchParams.get('id')!;
+				const updatedInvoice = await parseServer(ticketId);
 				setInvoice(updatedInvoice);
 			} else {
 				setInvoice(undefined);
--- src/app/ticket/view/parse/server.ts
+++ src/app/ticket/view/parse/server.ts
@@ -1,19 +1,23 @@
 'use server';
 
-import path from 'path';
-import { getDocument } from 'pdfjs-dist/legacy/build/pdf.mjs';
+import path from 'node:path';
+import * as PdfJs from 'pdfjs-dist/legacy/build/pdf.mjs';
+
 import { TicketInvoice } from '../tickets.model';
 import {
 	isValid as isMarkusTicket,
 	parse as parseMarkusTicket,
 } from './markus';
 
-export async function parse(): Promise<TicketInvoice> {
-	const pdfPath = path.resolve(process.cwd(), 'sample/4171657442.pdf');
+export async function parse(id: string): Promise<TicketInvoice> {
+	const pdfPath = path.resolve(process.cwd(), `sample/${id}.pdf`);
+
+	const { getDocument } = await PdfJs;
 	const pdf = await getDocument(pdfPath).promise;
 
 	if (await isMarkusTicket(pdf)) {
-		return await parseMarkusTicket(pdf);
+		const ticket = await parseMarkusTicket(pdf);
+		return JSON.parse(JSON.stringify(ticket));
 	}
 
 	throw new Error('Invalid ticket');
