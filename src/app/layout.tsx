8049a910d62dd468b5ea03aebae0a850bcc4e838
Kristaps <kristaps@austers.lv>
2024-01-04 03:30:18 +0200

    Declare LV language in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="en">
+		<html lang="lv">
 			<body className={inter.className}>
 				<Header />
 				{children}
