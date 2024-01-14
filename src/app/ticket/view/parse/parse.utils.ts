518f809a03e53f5f4a97f91a6662ceb0fb152287
Kristaps <kristaps@austers.lv>
2024-01-14 19:28:10 +0200

    Use base64 encoding for transporting image data

--- src/app/ticket/view/parse/parse.utils.ts
+++ src/app/ticket/view/parse/parse.utils.ts
@@ -21,13 +21,31 @@ export function encodeImage(imageData: ImageData): Scan['image'] {
 	return {
 		width: imageData.width,
 		height: imageData.height,
-		data: imageData.data.join(','),
+		data: encodeImageData(imageData.data),
 	};
+}
+
+function encodeImageData(data: Uint8ClampedArray): string {
+	const binaryString = new Uint8Array(data).reduce(
+		(acc, byte) => acc + String.fromCharCode(byte),
+		'',
+	);
+	return btoa(binaryString);
 }
 
 /** Decode an image from a string */
 export function decodeImage(data: Scan['image']): ImageData {
-	const imageDataArray = data.data.split(',').map(Number);
-	const imageData = Uint8ClampedArray.from(imageDataArray);
-	return new ImageData(imageData, data.width, data.height);
+	return new ImageData(decodeImageData(data.data), data.width, data.height);
+}
+
+function decodeImageData(data: string): Uint8ClampedArray {
+	const binaryString = atob(data);
+	const len = binaryString.length;
+
+	const bytes = new Uint8ClampedArray(len);
+	for (let i = 0; i < len; i++) {
+		bytes[i] = binaryString.charCodeAt(i);
+	}
+
+	return bytes;
 }
