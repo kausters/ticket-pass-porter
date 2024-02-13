5b3915f9e4399b4a03efaa344578b89247b67e1f
Kristaps <kristaps@austers.lv>
2024-02-13 03:14:41 +0200

    Round end times

--- src/app/ticket/parse/markus/load/index.ts
+++ src/app/ticket/parse/markus/load/index.ts
@@ -22,11 +22,24 @@ export async function load(
 	// Could not find show, return event duration
 	if (!show) {
 		const duration = event.lengthInMinutes + EXTRA_DURATION;
-		const end = start.plus({ minutes: duration }).toISO();
+		const endTime = start.plus({ minutes: duration });
+		const end = processTime(endTime).toISO();
 		return end ? { end } : {};
 	}
 
 	// Found show, return its actual end time
-	const end = DateTime.fromISO(show.dttmShowEndUTC).toISO();
+	const endTime = DateTime.fromISO(show.dttmShowEndUTC);
+	const end = processTime(endTime).toISO();
 	return end ? { end } : {};
+}
+
+/**
+ * For some reason, the API data often has Event durations and Show end times
+ * ending with "6" minutes. That's nasty on tickets and in calendar, so
+ * we round the calculated end time to the nearest 5 minutes.
+ */
+function processTime(time: DateTime): DateTime {
+	const minutes = time.minute;
+	const roundedMinutes = Math.round(minutes / 5) * 5;
+	return time.set({ minute: roundedMinutes });
 }
