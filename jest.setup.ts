646cf4009b2a880ae9e5e491c9f36e4b9d7e69ac
Kristaps <kristaps@austers.lv>
2024-04-06 21:14:04 +0300

    Configure Testing Library's test ID attribute

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,2 +1,9 @@
 import '@testing-library/jest-dom/jest-globals';
 import '@testing-library/jest-dom';
+
+import { configure } from '@testing-library/dom';
+
+configure({
+	// `data-testid` is barbaric, use `data-test-id` instead
+	testIdAttribute: 'data-test-id',
+});
