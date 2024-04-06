1c5925dcf4d655e8e47a1531080d85ad616192a9
Kristaps <kristaps@austers.lv>
2024-04-06 20:53:30 +0300

    Add Ticket Page tests

--- src/app/ticket/page.test.mock.tsx
+++ src/app/ticket/page.test.mock.tsx
@@ -1,11 +1,23 @@
 import { jest } from '@jest/globals';
+import { useSearchParams } from 'next/navigation';
+
+import TicketImport from './import';
+import TicketLoad from './load';
+import TicketView from './view';
+
+export const useSearchPramsMock: Pick<jest.Mocked<ReturnType<typeof useSearchParams>>, 'get'> = {
+	get: jest.fn(() => null),
+};
 
 jest.mock('next/navigation', () => ({
-	useSearchParams: () => ({
-		get: jest.fn(() => null),
-	}),
+	useSearchParams: () => useSearchPramsMock,
 }));
 
-jest.mock('./import', () => () => <div role="Import" />);
-jest.mock('./load', () => () => <div role="Load" />);
-jest.mock('./view', () => () => <div role="View" />);
+export const importMock: jest.Mocked<typeof TicketImport> = jest.fn(() => <></>);
+jest.mock('./import', () => importMock);
+
+export const loadMock: jest.Mocked<typeof TicketLoad> = jest.fn(() => <></>);
+jest.mock('./load', () => loadMock);
+
+export const viewMock: jest.Mocked<typeof TicketView> = jest.fn(() => <></>);
+jest.mock('./view', () => viewMock);
--- src/app/ticket/page.test.tsx
+++ src/app/ticket/page.test.tsx
@@ -1,13 +1,74 @@
 import './page.test.mock';
 
-import { describe, it } from '@jest/globals';
-import { render } from '@testing-library/react';
+import { describe, expect, it } from '@jest/globals';
+import { fireEvent, render } from '@testing-library/react';
 
 import TicketPage from './page';
+import { importMock, loadMock, useSearchPramsMock, viewMock } from './page.test.mock';
 
 describe('TicketPage', () => {
-	it('renders', async () => {
-		const result = render(<TicketPage />);
-		result.debug();
+	describe('Import', () => {
+		it('renders Import if no file or ID is present', () => {
+			// Import is rendered by default so we don't need to mock anything extra
+			importMock.mockReturnValue(<div data-test-id="import" />);
+			const { getByTestId } = render(<TicketPage />);
+
+			// Expect Import to be rendered
+			expect(importMock).toHaveBeenCalled();
+			expect(getByTestId('import')).toBeInTheDocument();
+		});
+	});
+
+	describe('Load', () => {
+		it('renders Load with ID from search params', () => {
+			// Mock the search params to return an ID, which should render Load
+			useSearchPramsMock.get.mockImplementation((key) => (key === 'id' ? '1' : null));
+
+			loadMock.mockReturnValue(<div data-test-id="load" />);
+			const { getByTestId } = render(<TicketPage />);
+
+			// Expect Load to be rendered with the ID from the search params
+			expect(loadMock).toHaveBeenCalledWith(expect.objectContaining({ id: '1' }), {});
+			expect(getByTestId('load')).toBeInTheDocument();
+		});
+
+		it('renders Load with file from Import', () => {
+			// Mock the Import component to call onImport with a file, which should render Load
+			const file = new File([], 'tickets.pdf');
+			importMock.mockImplementation(({ onImport }) => <button onClick={() => onImport(file)} />);
+
+			loadMock.mockReturnValue(<div data-test-id="load" />);
+			const { getByRole, getByTestId } = render(<TicketPage />);
+
+			// Click the Import button to trigger the file import
+			const importButton = getByRole('button');
+			fireEvent.click(importButton);
+
+			// Expect Load to be rendered with the file from Import
+			expect(loadMock).toHaveBeenCalledWith(expect.objectContaining({ file }), {});
+			expect(getByTestId('load')).toBeInTheDocument();
+		});
+	});
+
+	describe('View', () => {
+		it('renders View with invoice from Load', () => {
+			// Mock the search params to return an ID, which should render Load
+			useSearchPramsMock.get.mockImplementation((key) => (key === 'id' ? '1' : null));
+
+			// Mock the Load component to call onLoad with an invoice, which should render View
+			const invoice = { id: '1', tickets: [] };
+			loadMock.mockImplementation(({ onLoad }) => <button onClick={() => onLoad(invoice)} />);
+
+			viewMock.mockReturnValue(<div data-test-id="view" />);
+			const { getByRole, getByTestId } = render(<TicketPage />);
+
+			// Click the Load button to trigger the invoice load
+			const loadButton = getByRole('button');
+			fireEvent.click(loadButton);
+
+			// Expect View to be rendered with the invoice from Load
+			expect(viewMock).toHaveBeenCalledWith(expect.objectContaining({ invoice }), {});
+			expect(getByTestId('view')).toBeInTheDocument();
+		});
 	});
 });
