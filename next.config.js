14c8cc0e1acd519444877d6fd18941d7ecb5fa99
Kristaps Austers <kristaps@austers.lv>
2025-09-17 22:22:41 +0300

    Update to NextJS 15.5.3

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	experimental: {
-		serverComponentsExternalPackages: ['canvas', 'pdfjs-dist'],
-	},
+	serverExternalPackages: ['canvas', 'pdfjs-dist'],
 };
 
 module.exports = nextConfig;
