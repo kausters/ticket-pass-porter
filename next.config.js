d10380f98626776fe45f8fef62ed9d4d6edd6173
Kristaps <kristaps@austers.lv>
2025-09-17 23:08:59 +0300

    Update to NextJS 15 (#3)
    * Update to NextJS 15.5.3
    
    * Use TS Next config
    
    * Revert to Node 20
    
    * Update Jest & JSDom
    
    * Update Testing Library
    
    * Update React

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-	experimental: {
-		serverComponentsExternalPackages: ['canvas', 'pdfjs-dist'],
-	},
-};
-
-module.exports = nextConfig;
