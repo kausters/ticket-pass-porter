f31fa6c19a569a3275adebdb1b9c425026ef250b
Kristaps Austers <kristaps@austers.lv>
2025-09-18 21:22:56 +0300

    Move header from layout to index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import { Converter } from './(converter)/converter';
+import Header from './(home)/header/header';
 import styles from './page.module.css';
 
 export default function App() {
 	return (
-		<main className={styles.main}>
-			<Converter />
-		</main>
+		<>
+			<Header />
+			<main className={styles.main}>
+				<Converter />
+			</main>
+		</>
 	);
 }
