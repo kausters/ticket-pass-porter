404c20a053794a01eaf632bb8896c4cefe0ceced
Kristaps Austers <kristaps@austers.lv>
2025-09-25 03:24:02 +0300

    Control showing original with a button

--- src/app/(converter)/view/ticket/ticket.tsx
+++ src/app/(converter)/view/ticket/ticket.tsx
@@ -1,17 +1,26 @@
-import { FunctionComponent } from 'react';
+import clsx from 'clsx';
+import { FunctionComponent, useState } from 'react';
 
 import { Ticket } from '../../ticket.model';
 import TicketData from './data/ticket-data';
 import TicketImage from './image/ticket-image';
 import styles from './ticket.module.scss';
 
-const TicketComponent: FunctionComponent<{ data: Ticket }> = ({ data }) => (
-	<article className={styles.container}>
-		<div className={styles.ticket}>
-			<TicketData data={data} className={styles.data} />
-			<TicketImage data={data.image} className={styles.image} alt="Original ticket scan" />
-		</div>
-	</article>
-);
+const TicketComponent: FunctionComponent<{ data: Ticket }> = ({ data }) => {
+	const [showOriginal, setShowOriginal] = useState(false);
+
+	return (
+		<article className={styles.container}>
+			<div className={clsx(styles.ticket, showOriginal && styles.showOriginal)}>
+				<TicketData data={data} className={styles.data} />
+				<TicketImage data={data.image} className={styles.image} alt="Original ticket scan" />
+			</div>
+
+			<menu className={styles.menu}>
+				<button onClick={() => setShowOriginal(!showOriginal)}>Show original</button>
+			</menu>
+		</article>
+	);
+};
 
 export default TicketComponent;
