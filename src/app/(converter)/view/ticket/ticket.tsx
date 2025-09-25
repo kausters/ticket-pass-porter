6bc07ccc3c852e9789974874d6e5b3bb425dc31d
Kristaps Austers <kristaps@austers.lv>
2025-09-25 03:41:41 +0300

    Extract useToggleOriginal hook

--- src/app/(converter)/view/ticket/ticket.tsx
+++ src/app/(converter)/view/ticket/ticket.tsx
@@ -7,27 +7,13 @@ import TicketImage from './image/ticket-image';
 import styles from './ticket.module.scss';
 
 const TicketComponent: FunctionComponent<{ data: Ticket }> = ({ data }) => {
-	const [showOriginal, setShowOriginal] = useState(false);
-	const [hasRendered, setHasRendered] = useState(false);
-
-	function handleToggleOriginal() {
-		setShowOriginal(!showOriginal);
-		if (!hasRendered && !showOriginal) {
-			setHasRendered(true);
-		}
-	}
+	const [showOriginal, TicketOriginal, handleToggleOriginal] = useToggleOriginal();
 
 	return (
 		<article className={styles.container}>
 			<div className={clsx(styles.ticket, showOriginal && styles.showOriginal)}>
 				<TicketData data={data} className={styles.data} />
-				{(showOriginal || hasRendered) && (
-					<TicketImage
-						data={data.image}
-						className={clsx(styles.image, !showOriginal && styles.hidden)}
-						alt="Original ticket scan"
-					/>
-				)}
+				<TicketOriginal data={data.image} className={styles.image} alt="Original ticket scan" />
 			</div>
 
 			<menu className={styles.menu}>
@@ -38,3 +24,22 @@ const TicketComponent: FunctionComponent<{ data: Ticket }> = ({ data }) => {
 };
 
 export default TicketComponent;
+
+function useToggleOriginal() {
+	const [showOriginal, setShowOriginal] = useState(false);
+	const [hasRendered, setHasRendered] = useState(false);
+
+	function handleToggleOriginal() {
+		setShowOriginal(!showOriginal);
+		if (!hasRendered && !showOriginal) {
+			setHasRendered(true);
+		}
+	}
+
+	const TicketOriginal: typeof TicketImage = (props) => {
+		if (!showOriginal && !hasRendered) return null;
+		return <TicketImage {...props} />;
+	};
+
+	return [showOriginal, TicketOriginal, handleToggleOriginal] as const;
+}
