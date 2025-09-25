036d3e105a593b2eee3be537442aa806876867b0
Kristaps Austers <kristaps@austers.lv>
2025-09-25 03:31:55 +0300

    Keep rendering the original image after the 1st time

--- src/app/(converter)/view/ticket/ticket.tsx
+++ src/app/(converter)/view/ticket/ticket.tsx
@@ -8,16 +8,30 @@ import styles from './ticket.module.scss';
 
 const TicketComponent: FunctionComponent<{ data: Ticket }> = ({ data }) => {
 	const [showOriginal, setShowOriginal] = useState(false);
+	const [hasRendered, setHasRendered] = useState(false);
+
+	function handleToggleOriginal() {
+		setShowOriginal(!showOriginal);
+		if (!hasRendered && !showOriginal) {
+			setHasRendered(true);
+		}
+	}
 
 	return (
 		<article className={styles.container}>
 			<div className={clsx(styles.ticket, showOriginal && styles.showOriginal)}>
 				<TicketData data={data} className={styles.data} />
-				<TicketImage data={data.image} className={styles.image} alt="Original ticket scan" />
+				{(showOriginal || hasRendered) && (
+					<TicketImage
+						data={data.image}
+						className={clsx(styles.image, !showOriginal && styles.hidden)}
+						alt="Original ticket scan"
+					/>
+				)}
 			</div>
 
 			<menu className={styles.menu}>
-				<button onClick={() => setShowOriginal(!showOriginal)}>Show original</button>
+				<button onClick={handleToggleOriginal}>Show original</button>
 			</menu>
 		</article>
 	);
